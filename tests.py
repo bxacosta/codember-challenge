a73b32c911a12797c158e7daf05f58c82dabf443
Bryan Acosta <bryan-ax@hotmail.com>
2023-11-10 20:26:32 -0500

    test: add test for challenge_02

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from challenge_01 import decode
+from challenge_02 import compiler
 
 
 class TestChallenges(unittest.TestCase):
@@ -8,6 +9,11 @@ class TestChallenges(unittest.TestCase):
         self.assertEqual(decode("keys house HOUSE house keys"), 'keys2house3')
         self.assertEqual(decode("cup te a cup"), 'cup2te1a1')
         self.assertEqual(decode("houses house housess"), 'houses1house1housess1')
+
+    def test_challenge_02(self):
+        print("- Test Challenge 02")
+        self.assertEqual(compiler("##*&"), '4')
+        self.assertEqual(compiler("&##&*&@&"), '0243')
 
 
 if __name__ == '__main__':
